43c8cfb9833ac4bc09cc05353d17d7196818d3c9
whitgroves <whitney.groves@gmail.com>
2025-06-04 21:22:52 -0500

    managed supply token

---
+++ ManagedSupplyERC20.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.20;
+
+import "https://github.com/whitgroves/solidity-contracts/blob/main/Delegated.sol";
+
+// Imported code license: MIT
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+/* 
+ * An extension of OpenZeppelin's ERC20 which implements a manually adjustable tax, automatic burn rate,
+ * and restricted minting function, determined by the token's current supply.
+ */
+abstract contract ManagedSupplyERC20 is ERC20, Delegated, Pausable {
+
+    address public taxAddress;
+    uint8 public taxRate;
+    uint256 public targetSupply;
+
+    constructor(string memory name_, string memory symbol_, address initialOwner, uint256 targetSupply_) 
+        ERC20(name_, symbol_) 
+        Delegated(initialOwner) 
+    {
+        setTargetSupply(targetSupply_);
+    }
+
+    function setTargetSupply(uint256 targetSupply_) public virtual onlyOwner {
+        require (targetSupply_ > 0, "The target supply must be at least 1.");
+        targetSupply = targetSupply_;
+    }
+
+    function setTaxAddress(address taxAddress_) public virtual onlyOwner {
+        if (taxAddress_ == address(0)) revert("Tax address cannot be zero address.");
+        taxAddress = taxAddress_;
+    }
+
+    function setTaxRate(uint8 taxRate_) external virtual onlyDelegate {
+        if (taxRate_ > (20 - burnRate())) revert("Tax + burn rates cannot exceed 20%.");
+        taxRate = taxRate_;
+    }
+
+    function burnRate() public virtual view returns (uint) {
+        uint targetRate = totalSupply() / targetSupply;
+        if (targetRate > 0 && targetRate > (20 - taxRate)) targetRate = (20 - taxRate);
+        return targetRate;
+    }
+
+    function _inflated() internal virtual view returns (bool) {
+        return totalSupply() > targetSupply;
+    }
+
+    // Takes a payment value, deducts and transfers a % of it as tax and/or burn, and then returns the remainder.
+    function _adjust(address account, uint256 value) internal virtual whenNotPaused returns (uint256) {
+        uint remainder = value;
+        if (taxRate > 0 && taxAddress != address(0)) {
+            uint tax = (taxRate * value) / 100;
+            _transfer(account, payable(taxAddress), tax);
+            remainder -= tax;
+        }
+        if (_inflated()) {
+            uint burn = (burnRate() * value) / 100;
+            _burn(account, burn);
+            remainder -= burn;
+        }
+        return remainder;
+    }
+
+    // Override of ERC20.transfer() to tax and burn each transaction, when applicable.
+    function transfer(address to, uint256 value) public virtual override returns (bool) {
+        address sender = _msgSender();
+        _transfer(sender, to, _adjust(sender, value));
+        return true;
+    }
+
+    // Override of ERC20.transferFrom() to tax and burn each transaction, when applicable.
+    function transferFrom(address from, address to, uint256 value) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, _adjust(spender, value));
+        return true;
+    }
+
+    // Public wrapper for ERC20._mint() so owner and delegates can mint tokens while enforcing the supply cap.
+    function mint(address account, uint256 value) public virtual onlyDelegate {
+        if (_inflated()) revert("Cannot mint while token supply is inflated.");
+        uint maxValue = targetSupply - totalSupply();
+        if (value > maxValue) revert("Minting requested value would exceed target supply.");
+        _mint(account, value);
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ These contracts have been unlicensed and are freely available for any use, but b
 
 ## Contract Extensions
 
-### Delegated.sol
+### Delegated
 An extension of OpenZeppelin's Ownable contract to allow for delegated calls to contract functions. 
 Makes the `onlyDelegate` modifier available for use, similar to `onlyOwner`:
 
@@ -25,7 +25,21 @@ contract MyContract is Delegated {
 
 ## Standalone Contracts
 
-### StakingPool.sol
+### StakingPool
 A revision of [StakingPool](https://github.com/whitgroves/staking-pool) using inherited access and emergency stop controls. Note that in this version, the only way to destake a pool is to retire it.
 
 To use, deploy the contract pointing to the contract address for the ERC20 token you want to setup staking for, then transfer and distribute funds as needed. Secondary contracts can be added as delegates to automate the distribution process entirely on-chain.
+
+### ManagedSupplyERC20
+An extension of OpenZeppelin's ERC20 which implements a manually adjustable tax, automatic burn rate, and a delegated minting function, which is restricted by the token's target supply. The contract is abstract, but can be subclassed and deployed rather easily:
+```
+import "https://github.com/whitgroves/solidity-contracts/blob/main/ManagedSupplyERC20.sol";
+
+contract TestToken is ManagedSupplyERC20 {
+    constructor() ManagedSupplyERC20("Your Own Distributed Ledger", "YODL", <initial owner>, <target supply>) {
+        _mint(<initial holder>, <initial supply>);
+    }
+}
+```
+
+Note that while the tax rate is adjustable, the contract will soft-enforce a 20% tax + burn rate on transactions; if this is not desired, you will need to override `setTaxRate()` and `burnRate()` in your implementation, or raise the target supply cap until the burn rate drops. 
